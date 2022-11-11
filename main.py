c92de058395a0d71c7ff9fc059fe441e3d945ea3
Alexander Aldridge <alexander@aldridge.ca>
2022-11-11 15:47:37 -0500

    Improved reaction handling

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # This example requires the 'members' privileged intent to use the Member converter
 # and the 'message_content' privileged intent for prefixed commands.
 
-import random
 import requests
 import discord
+from discord import *
 from discord.ext import commands
 
 description = """
@@ -13,6 +13,7 @@ There are a number of utility commands being showcased here.
 
 intents = discord.Intents.default()
 intents.members = True
+intents.messages = True
 intents.message_content = True
 
 bot = commands.Bot(
@@ -40,6 +41,24 @@ async def getcat(ctx):
     else:
         resp = "I was unable to catch the cat for a picture... Try again later"
     await ctx.respond(resp)
+
+@bot.event
+async def on_message(message: Message):
+    if message.author.id == bot.user.id:
+        return
+    
+    if (message.type in (MessageType.default, MessageType.reply)):
+        await doReactions(message)
+
+
+async def doReactions(message: Message):
+
+    if ('vote' in message.content.lower()):
+        await message.reply('I love democracy')
+        return
+    if ('linux' in message.content.lower() and 'gnu' not in message.content.lower()):
+        await message.reply('Did you mean GNU Linux?')
+
 
 
 with open('auth_token.txt') as f:
