70c9be78eb01a103955b0b4293a7d9c61093b386
Alexander Aldridge <alexander@aldridge.ca>
2025-05-09 18:04:17 -0400

    Use regex word boundaries in trigger matching

--- reply.py
+++ reply.py
@@ -1,46 +1,59 @@
+import re
+
 from discord import Message
 
-'''
+"""
     Super class for all our message reply triggers
-'''
+"""
+
+
 class ReplyTrigger:
     def __init__(self, triggers: list[str], exceptTriggers: list[str] = []):
         self.triggers = tuple(map(lambda s: s.lower(), triggers))
         self.exceptTriggers = tuple(map(lambda s: s.lower(), exceptTriggers))
 
     def isTriggered(self, messageContent: str) -> bool:
-        messageContent = messageContent.lower().strip()
+        messageContent = messageContent.lower()
 
         for trigger in self.exceptTriggers:
-            if (trigger in messageContent):
+            if re.search(f"\b{trigger}\b", messageContent) is not None:
                 return False
 
         for trigger in self.triggers:
-            if (trigger in messageContent):
+            if re.search(f"\b{trigger}\b", messageContent) is not None:
                 return True
+
         return False
-    
+
     async def doReply(self, message: Message):
         pass
 
-'''
+
+"""
     Replies with only an imageUrl (discord will just display the image)
-'''
+"""
+
+
 class ImageReplyTrigger(ReplyTrigger):
-    def __init__(self, triggers: list[str], imageURL: str, exceptTriggers: list[str] = []):
+    def __init__(
+        self, triggers: list[str], imageURL: str, exceptTriggers: list[str] = []
+    ):
         super().__init__(triggers, exceptTriggers)
         self.imageUrl = imageURL
-    
+
     async def doReply(self, message: Message):
         await message.reply(self.imageUrl)
 
-'''
+
+"""
     Replies with a text response
-'''
+"""
+
+
 class TextReplyTrigger(ReplyTrigger):
     def __init__(self, triggers: list[str], reply: str, exceptTriggers: list[str] = []):
         super().__init__(triggers, exceptTriggers)
         self.reply = reply
-    
+
     async def doReply(self, message: Message):
         await message.reply(self.reply)
